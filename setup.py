3da667a909c9fd038f7efcc19918a034f1fccff4
paulhendricks <paul.hendricks.2013@owu.edu>
2015-09-29 10:43:21 -0400

    added some functions

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 __author__ = 'phendricks'
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/paulhendricks/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
